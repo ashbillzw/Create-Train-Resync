8c119944d2401aefd43b14475426924dd407baba
AshBill <me@ashbill.com>
2025-04-11 15:19:37 -0400

    :sparkles: Automatically trigger resync

--- TrainResync/src/main/java/com/ashbill/trainresync/TrainResyncMod.java
+++ TrainResync/src/main/java/com/ashbill/trainresync/TrainResyncMod.java
@@ -1,45 +1,75 @@
 package com.ashbill.trainresync;
 
-import com.mojang.brigadier.context.CommandContext;
 import com.simibubi.create.content.trains.entity.CarriageContraptionEntity;
 import net.minecraft.client.Minecraft;
-import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterClientCommandsEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+
 @Mod("trainresync")
+@Mod.EventBusSubscriber(modid = "trainresync", value = Dist.CLIENT)
 public class TrainResyncMod {
+    private static ResourceKey<Level> lastDimension = null;
+    private static int ticksUntilSync = -1;
 
     public TrainResyncMod() {
-        IEventBus bus = MinecraftForge.EVENT_BUS;
-        bus.addListener(this::onClientCommandRegister);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void onClientCommandRegister(RegisterClientCommandsEvent event) {
+    @SubscribeEvent
+    public void onClientCommandRegister(RegisterClientCommandsEvent event) {
         event.getDispatcher().register(
-            Commands.literal("shuaxin").executes(TrainResyncMod::resyncCarriages)
+            Commands.literal("shuaxin")
+                .executes(ctx -> resyncCarriages())
         );
         event.getDispatcher().register(
-            Commands.literal("sx").executes(TrainResyncMod::resyncCarriages)
+            Commands.literal("sx")
+                .executes(ctx -> resyncCarriages())
         );
     }
 
-    private static int resyncCarriages(CommandContext<CommandSourceStack> context) {
-        var mc = Minecraft.getInstance();
+    @SubscribeEvent
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.END)
+            return;
+
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.level == null || mc.player == null)
+            return;
+        
+        ResourceKey<Level> currentDimension = mc.level.dimension();
+
+        if (lastDimension == Level.NETHER && currentDimension == Level.OVERWORLD) {
+            ticksUntilSync = 60;
+            mc.player.sendSystemMessage(Component.literal("[AshBill TrainResync] 3秒后将刷新列车状态"));
+        }
+
+        if (ticksUntilSync > 0) {
+            ticksUntilSync--;
+            if (ticksUntilSync == 0) resyncCarriages();
+        }
+
+        lastDimension = currentDimension;
+    }
+
+    private static int resyncCarriages() {
+        Minecraft mc = Minecraft.getInstance();
         var player = mc.player;
         if (player == null || mc.level == null)
             return 0;
 
-        int moved = 0;
         for (Entity e : mc.level.entitiesForRendering()) {
             if (e instanceof CarriageContraptionEntity) {
-                e.setPos(player.getX(), player.getY(), player.getZ());
-                moved++;
+                e.setPos(player.getX(), player.getY() - 512, player.getZ());
             }
         }
 
