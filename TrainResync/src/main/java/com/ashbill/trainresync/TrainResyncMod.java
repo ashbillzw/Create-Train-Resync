763b2691e360a1f2f2d3b091e9a113ecfbd0475c
AshBill <me@ashbill.com>
2025-10-15 21:39:31 -0400

    🐛 Actually figure out mixin

--- TrainResync/src/main/java/com/ashbill/trainresync/TrainResyncMod.java
+++ TrainResync/src/main/java/com/ashbill/trainresync/TrainResyncMod.java
@@ -1,7 +1,6 @@
 package com.ashbill.trainresync;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 
--- TrainResync/src/main/java/com/ashbill/trainresync/mixin/FakeSeatMixin.java
+++ TrainResync/src/main/java/com/ashbill/trainresync/mixin/FakeSeatMixin.java
@@ -3,25 +3,27 @@ package com.ashbill.trainresync.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.sugar.Local;
 
 import net.minecraft.world.level.block.state.BlockState;
 
 import com.simibubi.create.content.contraptions.Contraption;
+import com.simibubi.create.content.contraptions.actors.seat.SeatBlock;
 
 import com.ashbill.trainresync.tags.TRTags;
 
 @Mixin(Contraption.class)
 public abstract class FakeSeatMixin {
+    @Definition(id = "SeatBlock", type = SeatBlock.class)
+    @Expression("? instanceof SeatBlock")
     @ModifyExpressionValue(
         method = "moveBlock",
-        at = @At(
-            value = "INSTANCEOF",
-            target = "Lcom.simibubi.create.content.contraptions.actors.seat.SeatBlock;"
-        )
+        at = @At("MIXINEXTRAS:EXPRESSION"),
+        remap = false
     )
     private boolean trainresync$seatOrFakeSeat(boolean original, @Local BlockState state) {
-        System.out.println("[TrainResync] FakeSeatMixin ran!");
         return original || state.is(TRTags.FAKE_SEATS);
     }
 }
