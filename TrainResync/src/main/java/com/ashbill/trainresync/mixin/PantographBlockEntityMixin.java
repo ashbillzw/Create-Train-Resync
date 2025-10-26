a84678a5b7c61029b1fb1df64fcf499c4c47e6b2
AshBill <me@ashbill.com>
2025-10-25 23:28:52 -0400

    ✨ Add animation hult for /shuaxin

---
+++ TrainResync/src/main/java/com/ashbill/trainresync/mixin/PantographBlockEntityMixin.java
@@ -0,0 +1,38 @@
+package com.ashbill.trainresync.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.At;
+
+import de.mrjulsen.paw.blockentity.PantographBlockEntity;
+
+
+@Mixin(value = PantographBlockEntity.class, remap = false)
+public abstract class PantographBlockEntityMixin {
+    @Shadow private double catenaryWireHeight;
+    @Unique private double trainresync$prevCatenaryWireHeight = 0.0D;
+    @Unique private int trainresync$retractionBufferTicks = 0;
+
+    @ModifyArg(
+        method = "updateContraptionValues",
+        at = @At(
+            value = "INVOKE",
+            target = "Lde/mrjulsen/paw/blockentity/PantographBlockEntity;setExpanded(Z)V"
+        )
+    )
+    private boolean trainresync$delayRetraction(boolean original) {
+        if (original) {
+            trainresync$retractionBufferTicks = 5;
+            trainresync$prevCatenaryWireHeight = catenaryWireHeight;
+            return true;
+        }
+        else if (trainresync$retractionBufferTicks > 0) {
+            trainresync$retractionBufferTicks--;
+            catenaryWireHeight = trainresync$prevCatenaryWireHeight;
+            return true;
+        }
+        return false;
+    }
+}
