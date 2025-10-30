63d89937b8fc73550a80814285e865e7ab70a134
AshBill <me@ashbill.com>
2025-10-30 03:10:29 -0400

    :art: Improve format of PantographBlockEntityMixin

--- TrainResync/src/main/java/com/ashbill/trainresync/mixin/PantographBlockEntityMixin.java
+++ TrainResync/src/main/java/com/ashbill/trainresync/mixin/PantographBlockEntityMixin.java
@@ -6,14 +6,24 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+
 import de.mrjulsen.paw.blockentity.PantographBlockEntity;
 
 
 @Mixin(value = PantographBlockEntity.class, remap = false)
-public abstract class PantographBlockEntityMixin {
+public abstract class PantographBlockEntityMixin extends SmartBlockEntity {
     @Shadow private double catenaryWireHeight;
     @Unique private double trainresync$prevCatenaryWireHeight = 0.0D;
     @Unique private int trainresync$retractionBufferTicks = 0;
+
+    public PantographBlockEntityMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
 
     @ModifyArg(
         method = "updateContraptionValues",
