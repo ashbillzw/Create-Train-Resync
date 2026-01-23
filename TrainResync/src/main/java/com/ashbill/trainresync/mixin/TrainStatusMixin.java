b316e40a604c872a9cc4940ba6990dafcd68e87e
AshBill <me@ashbill.com>
2026-01-22 23:28:00 -0500

    🐛 ✏️ Fix Typo

--- TrainResync/src/main/java/com/ashbill/trainresync/mixin/TrainStatusMixin.java
+++ TrainResync/src/main/java/com/ashbill/trainresync/mixin/TrainStatusMixin.java
@@ -10,8 +10,8 @@ import com.simibubi.create.content.trains.entity.TrainStatus;
 
 @Mixin(value = TrainStatus.class, remap = false)
 public abstract class TrainStatusMixin {
-    @Inject(method = "doublePortal", at = @At("HEAD"))
-    private static void trainresync$silenceDoublePortalWarning(CallbackInfo ci) {
+    @Inject(method = "doublePortal", at = @At("HEAD"), cancellable = true)
+    private void trainresync$silenceDoublePortalWarning(CallbackInfo ci) {
         System.out.println("[AshBill] double_portal warning suppressed.");
         ci.cancel();
     }
