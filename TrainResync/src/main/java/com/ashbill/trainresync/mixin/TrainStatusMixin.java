438aff2e7c11ec0b81b5c4494711d19409ec11cf
AshBill <me@ashbill.com>
2026-08-02 21:13:08 +0800

    ✨ Excessive double_portal warning messages will now be collapsed

--- TrainResync/src/main/java/com/ashbill/trainresync/mixin/TrainStatusMixin.java
+++ TrainResync/src/main/java/com/ashbill/trainresync/mixin/TrainStatusMixin.java
@@ -1,18 +1,41 @@
 package com.ashbill.trainresync.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.server.ServerLifecycleHooks;
 
 import com.simibubi.create.content.trains.entity.TrainStatus;
 
 
 @Mixin(value = TrainStatus.class, remap = false)
 public abstract class TrainStatusMixin {
+    @Unique private int trainresync$lastPrintedWarningTime = -100;
+    @Unique private int trainresync$collapsedWarnings = 0;
+
     @Inject(method = "doublePortal", at = @At("HEAD"), cancellable = true)
     private void trainresync$silenceDoublePortalWarning(CallbackInfo ci) {
-        System.out.println("[AshBill] double_portal warning suppressed.");
+        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+        if (server == null) return;
+
+        int currentTick = server.getTickCount();
+
+        if (currentTick - trainresync$lastPrintedWarningTime > 99) {
+            String warningMessage = "[AshBill] double_portal warning suppressed.";
+            if (trainresync$collapsedWarnings > 0) {
+                warningMessage += " +" + trainresync$collapsedWarnings + " warnings collapsed.";
+            }
+            System.out.println(warningMessage);
+            trainresync$lastPrintedWarningTime = currentTick;
+            trainresync$collapsedWarnings = 0;
+        }
+        else {
+            trainresync$collapsedWarnings++;
+        }
         ci.cancel();
     }
 }
